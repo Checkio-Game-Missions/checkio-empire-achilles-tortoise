7e9f47b1fd642e1367361e497330b330af4cd594
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-16 17:14:54 +0300

    add representation

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,12 +1,13 @@
 from checkio_referee import RefereeBase
-from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.validators import FloatEqualValidator
+from checkio_referee import covercodes, validators, representations
 
 import settings
 import settings_env
 from tests import TESTS
 
-FloatEqualValidator.PRECISION = 8
+Validator = validators.FloatEqualValidator
+
+Validator.PRECISION = 8
 
 
 class Referee(RefereeBase):
@@ -14,9 +15,14 @@ class Referee(RefereeBase):
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "chase"
-    VALIDATOR = FloatEqualValidator
+    VALIDATOR = Validator
+    CALLED_REPRESENTATIONS = {
+        "python_3": representations.unwrap_arg_representation,
+        "python_2": representations.unwrap_arg_representation,
+        "javascript": representations.unwrap_arg_representation
+    }
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
         "javascript": None
     }
